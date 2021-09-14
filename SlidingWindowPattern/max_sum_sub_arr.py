bcd3ed80020e9fa496116099d1eb79e0a1d57d1c
Abdul Salam K A <abdul.salam.k.a@intel.com>
2021-09-15 00:03:07 +0530

    comment updated

--- SlidingWindowPattern/max_sum_sub_arr.py
+++ SlidingWindowPattern/max_sum_sub_arr.py
@@ -6,6 +6,7 @@ Explanation: Subarray with maximum sum is [5, 1, 3].
 Input: [2, 3, 4, 1, 5], k=2 
 Output: 7
 Explanation: Subarray with maximum sum is [3, 4].
+using python
 '''
 def max_sub_array_of_size_k(k,arr):
     max_sum, window_sum=0, 0
