c4aafd764c6626288e06e3fdde7a8ff2d767433d
Abdul Salam K A <32676744+abduzalam@users.noreply.github.com>
2021-09-20 12:50:49 +0530

    Create length_of_longest_no_repeating_substring.py

---
+++ SlidingWindowPattern/length_of_longest_no_repeating_substring.py
@@ -0,0 +1,27 @@
+'''
+Given a string, find the length of the longest substring which has no repeating characters
+Ex:1
+
+Input String : "aabccbb"
+Output :3
+Explanation : The longest substring without any repeating char is "abc"
+'''
+
+def lengthof_longest_substring_with_no_repeat(instr):
+    char_index_map = {}
+    window_start = 0
+    max_length = 0
+
+    for window_end in range(len(instr)):
+        right_char = instr[window_end]
+        if right_char in char_index_map:
+            window_start = max(window_start,char_index_map[right_char]+1)
+        char_index_map[right_char] = window_end
+        max_length = max(max_length,window_end-window_start + 1)
+    return max_length
+        
+
+            
+def main():
+    print("length of longest substring with no repeat = "+str(lengthof_longest_substring_with_no_repeat("aabccbb")))
+main()
